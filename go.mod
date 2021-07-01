910f7e3191e8041c4aa6fb5f82c0433d68fdacf2
chaocai2001 <chaocai2001@icloud.com>
2021-07-01 14:52:55 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chaocai2001/build-demo
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	greeting := os.Getenv("GREETING")
+	if greeting == "" {
+		greeting = "Hello auto-pack."
+	}
+	io.WriteString(w, greeting)
+}
+
+func main() {
+	http.HandleFunc("/hello", HelloServer)
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
